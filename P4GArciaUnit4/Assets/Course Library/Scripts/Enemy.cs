df357f7793d9385cbb61b6186cd4dcf6b15eb796
AwesomeSauce-67 <750433@student.dusd.net>
2026-04-13 15:27:51 -0700

    added project and almost finsihed 4.3

---
+++ P4GArciaUnit4/Assets/Course Library/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position);
+        enemyRb.AddForce(lookDirection *  speed);
+    }
+}
---
+++ P4GArciaUnit4/Assets/Course Library/Scripts/PlayerScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15;
+    public GameObject powerupIndicator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fowardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * fowardInput);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.SetActive(true);
+           
+        }
+    }
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false; 
+        powerupIndicator.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer = collision.gameObject.transform.position - transform.position);
+        }
+    }
+}
